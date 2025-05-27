06a3e68febfdde699d29037dd71e03b1ea0145f9
Toreole <toreole.official@gmail.com>
2025-05-27 19:07:23 +0200

    IDE format of code files

--- Assets/Scripts/TalentGraphLayout.cs
+++ Assets/Scripts/TalentGraphLayout.cs
@@ -1,9 +1,7 @@
-﻿using System.Collections.Generic;
-using System.Collections;
+﻿using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
-using UnityEngine.XR;
 
 public class TalentGraphLayout : MonoBehaviour
 {
@@ -33,58 +31,58 @@ public class TalentGraphLayout : MonoBehaviour
         if (graph == null)
             yield break;
         _lastGraph = graph;
-		nodes = graph.Nodes;
+        nodes = graph.Nodes;
         // order of operations is absolutely vital:
-		//ShuffleNodes(); 
+        ShuffleNodes();
         // shuffle before doing any other calculations.
-		CalculateNodeDepths();
+        CalculateNodeDepths();
         yield return null;
 
         // instantiating the nodes.
         foreach (var node in nodes.OrderBy(x => x.RuntimeLevel))
         {
             var instance = Instantiate(nodePrefab, transform).GetComponent<RectTransform>();
-            instance.anchoredPosition = new Vector2(0, -preferredSpacing *  (node.RuntimeLevel + 1));
+            instance.anchoredPosition = new Vector2(0, -preferredSpacing * (node.RuntimeLevel + 1));
             node.RuntimeInstance = instance;
             instance.name = node.Name + $" (l:{node.RuntimeLevel}, c:{node.NodeClass}, i:[{node.NodeIndex}], p:[{string.Join(",", node.ParentIndices)}])";
-			//  Debug.Log(instance.sizeDelta); 50x50
-		}
+            //  Debug.Log(instance.sizeDelta); 50x50
+        }
         Debug.Log("instantiated");
-		yield return null;
+        yield return null;
 
-		//now do layout.
-		//goal is that nodes dont overlap one another.
-		//simple first step: spread out the nodes on each level
-		// desired params: 
-		// - level spacing
-		// - max width (for the entire graph)
-		// - minimum horizontal spacing
+        //now do layout.
+        //goal is that nodes dont overlap one another.
+        //simple first step: spread out the nodes on each level
+        // desired params: 
+        // - level spacing
+        // - max width (for the entire graph)
+        // - minimum horizontal spacing
 
-		// how to ensure minimum entropy (less messy?)
-		// min total node distance?
+        // how to ensure minimum entropy (less messy?)
+        // min total node distance?
 
-		// base positioning around all inner nodes (which have parents and children)
-		var offset = -(layoutWidth / 2f);
+        // base positioning around all inner nodes (which have parents and children)
+        var offset = -(layoutWidth / 2f);
         //groups will be ordered by depth ascending.
         var nodeGroups = nodes.GroupBy(x => x.RuntimeLevel).OrderBy(g => g.Key).Select(g => g.ToArray()).ToArray();
-		foreach (var group in nodeGroups)
+        foreach (var group in nodeGroups)
         {
             int i = 1;
             var groupSize = group.Length;
 
-            foreach(var node in group)
+            foreach (var node in group)
             {
                 var justifiedPos = (layoutWidth / (groupSize + 1)) * i++;
                 var pos = node.RuntimeInstance.anchoredPosition;
                 pos.x = justifiedPos + offset;
                 node.RuntimeInstance.anchoredPosition = pos;
             }
-		}
-		Debug.Log("groups formed");
-		yield return null;
+        }
+        Debug.Log("groups formed");
+        yield return null;
 
-		// reverse edges:
-		int[][] childNodes = new int[nodes.Length][];
+        // reverse edges:
+        int[][] childNodes = new int[nodes.Length][];
         for (int i = 0; i < nodes.Length; i++)
         {
             List<int> children = new List<int>();
@@ -94,51 +92,54 @@ public class TalentGraphLayout : MonoBehaviour
                     children.Add(j);
             }
             childNodes[i] = children.ToArray();
-		}
-		Debug.Log("reverse edges");
-		yield return null;
+        }
+        Debug.Log("reverse edges");
+        yield return null;
 
-		// hard layout.
-		for (int gi = 0; gi < nodeGroups.Length; gi++)
+        // hard layout.
+        for (int gi = 0; gi < nodeGroups.Length; gi++)
         {
             var group = nodeGroups[gi];
             int groupSize = group.Length;
-            for(int i = 0; i < groupSize; i++) 
+            for (int i = 0; i < groupSize; i++)
             {
                 var node = group[i];
                 node.RuntimeInstance.anchoredPosition = new Vector2(preferredSpacing * i, -preferredSpacing * gi); // gi == depth;
             }
-		}
-		Debug.Log("basic layout");
-		yield return null;
-		// minimize crossings in each layer after the first.
-		// crossings can only happen, when the nodes have at least one different parent.
-		for (int gi = 1; gi < nodeGroups.Length; gi++)
-		{
-			var group = nodeGroups[gi];
-			int groupSize = group.Length;
-			for (int i = 0; i < groupSize; i++)
-			{
-				var node = group[i];
+        }
+        Debug.Log("basic layout");
+        yield return null;
+        // minimize crossings in each layer after the first.
+        // crossings can only happen, when the nodes have at least one different parent.
+        for (int gi = 1; gi < nodeGroups.Length; gi++)
+        {
+            var group = nodeGroups[gi];
+            int groupSize = group.Length;
+            for (int i = 0; i < groupSize; i++)
+            {
+                var node = group[i];
                 var nodeTransform = node.RuntimeInstance;
                 var iParents = node.ParentIndices;
-				for (int j = i + 1; j < groupSize; j++)
-				{
-					var otherNode = group[j];
+                for (int j = i + 1; j < groupSize; j++)
+                {
+                    var otherNode = group[j];
                     var otherNodeTransform = otherNode.RuntimeInstance;
-					var jParents = otherNode.ParentIndices;
+                    var jParents = otherNode.ParentIndices;
 
-					for (int pi = 0; pi < iParents.Length; pi++)
+                    for (int pi = 0; pi < iParents.Length; pi++)
                     {
-						for (int pj = 0; pj < jParents.Length; pj++)
+                        for (int pj = 0; pj < jParents.Length; pj++)
                         {
                             if (iParents[pi] == jParents[pj])
                                 continue;
                             var ipPos = nodes[iParents[pi]].RuntimeInstance.anchoredPosition;
                             var iPos = nodeTransform.anchoredPosition;
                             var jpPos = nodes[jParents[pj]].RuntimeInstance.anchoredPosition;
-							var jPos = otherNodeTransform.anchoredPosition;
-							if ((ipPos.x < jpPos.x && iPos.x > jPos.x) || (ipPos.x > jpPos.x && iPos.x < jPos.x))
+                            var jPos = otherNodeTransform.anchoredPosition;
+                            // swap to nodes positions to remove a crossing.
+                            // this only works out when it is possible.
+                            // if there still are crossings in this layer after this step, the parent sub-graphs need to be re-ordered.
+                            if ((ipPos.x < jpPos.x && iPos.x > jPos.x) || (ipPos.x > jpPos.x && iPos.x < jPos.x))
                             {
                                 node.RuntimeInstance.anchoredPosition = jPos;
                                 otherNode.RuntimeInstance.anchoredPosition = iPos;
@@ -146,30 +147,31 @@ public class TalentGraphLayout : MonoBehaviour
                         }
                     }
                 }
-			}
-		}
-		Debug.Log("minimized crossings");
-		yield return null;
-	}
+            }
+        }
+        Debug.Log("minimized crossings");
+        yield return null;
+    }
 
     void Update()
-	{
-		if (graph != _lastGraph)
-		{
-			for (int i = transform.childCount - 1; i >= 0; i--)
-				Destroy(transform.GetChild(i).gameObject);
+    {
+        if (graph != _lastGraph)
+        {
+            for (int i = transform.childCount - 1; i >= 0; i--)
+                Destroy(transform.GetChild(i).gameObject);
 
-			StartCoroutine(OnEnable());
-		}
-		if (nodes == null)
+            StartCoroutine(OnEnable());
+        }
+        if (nodes == null)
             return;
 
-        foreach(var node in nodes)
+        foreach (var node in nodes)
         {
-            foreach(var pi in node.ParentIndices) {
+            foreach (var pi in node.ParentIndices)
+            {
                 Debug.DrawLine(node.RuntimeInstance.position, nodes[pi].RuntimeInstance.position);
-			}
-		}
+            }
+        }
     }
 
     // ! this needs to be called first !
@@ -179,17 +181,17 @@ public class TalentGraphLayout : MonoBehaviour
         {
             int a = Random.Range(0, nodes.Length);
             int b = Random.Range(0, nodes.Length);
-			(nodes[b], nodes[a]) = (nodes[a], nodes[b]);
+            (nodes[b], nodes[a]) = (nodes[a], nodes[b]);
 
-			foreach (var node in nodes)
+            foreach (var node in nodes)
             {
                 var indices = node.ParentIndices;
                 for (int pi = 0; pi < indices.Length; pi++)
                 {
                     if (indices[pi] == a)
-						indices[pi] = b;
+                        indices[pi] = b;
                     else if (indices[pi] == b)
-						indices[pi] = a;
+                        indices[pi] = a;
                 }
                 node.ParentIndices = indices;
             }
@@ -201,40 +203,40 @@ public class TalentGraphLayout : MonoBehaviour
     {
         foreach (var node in nodes)
             node.RuntimeLevel = 0;
-		// highly inefficient:
-		bool changesMade;
-		do
+        // highly inefficient:
+        bool changesMade;
+        do
         {
             changesMade = false;
-            
+
             for (int i = 0; i < nodes.Length; i++)
             {
                 var node = nodes[i];
-				foreach (var pi in node.ParentIndices)
-				{
+                foreach (var pi in node.ParentIndices)
+                {
                     var parentNode = nodes[pi];
-					parentNode.NodeClass = nodes[pi].HasParent ? NodeClass.Inner : NodeClass.Root;
-					if (parentNode.RuntimeLevel >= node.RuntimeLevel)
-					{
+                    parentNode.NodeClass = nodes[pi].HasParent ? NodeClass.Inner : NodeClass.Root;
+                    if (parentNode.RuntimeLevel >= node.RuntimeLevel)
+                    {
                         // a node should always have a higher level than its parents
-						node.RuntimeLevel = nodes[pi].RuntimeLevel + 1;
-						changesMade = true;
+                        node.RuntimeLevel = nodes[pi].RuntimeLevel + 1;
+                        changesMade = true;
                         // sanity check
-						if (parentNode.RuntimeLevel >= nodes.Length - 1)
-						{
-							Debug.LogError("why");
-							return;
-						}
-					}
+                        if (parentNode.RuntimeLevel >= nodes.Length - 1)
+                        {
+                            Debug.LogError("why");
+                            return;
+                        }
+                    }
                 }
             }
         } while (changesMade);
         for (int i = 0; i < nodes.Length; i++)
         {
             var node = nodes[i];
-			if (node.NodeClass == NodeClass.Unknown)
-				node.NodeClass = NodeClass.Leaf;
+            if (node.NodeClass == NodeClass.Unknown)
+                node.NodeClass = NodeClass.Leaf;
             node.NodeIndex = i;
-		}
-	}
+        }
+    }
 }
--- Assets/Scripts/TalentTreeGraph.cs
+++ Assets/Scripts/TalentTreeGraph.cs
@@ -1,69 +1,67 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 [Serializable]
 [CreateAssetMenu(fileName = "new TalentTreeGraph", menuName = "Custom/Talents/Graph")]
 public class TalentTreeGraph : ScriptableObject
 {
-	[SerializeField]
-	private TalentNode[] nodes = new TalentNode[0];
+    [SerializeField]
+    private TalentNode[] nodes = new TalentNode[0];
 
-	public TalentNode[] Nodes => GetCopyOfNodes();
+    public TalentNode[] Nodes => GetCopyOfNodes();
 
-	// deep clone of the data for safety.
-	public TalentNode[] GetCopyOfNodes()
-	{
-		var result = new TalentNode[nodes.Length];
-		for (int i = 0; i < nodes.Length; i++)
-		{
-			var node = new TalentNode(nodes[i]);
-			result[i] = node;
-		}
-		return result;
-	}
+    // deep clone of the data for safety.
+    public TalentNode[] GetCopyOfNodes()
+    {
+        var result = new TalentNode[nodes.Length];
+        for (int i = 0; i < nodes.Length; i++)
+        {
+            var node = new TalentNode(nodes[i]);
+            result[i] = node;
+        }
+        return result;
+    }
 }
 
 [Serializable]
 public class TalentNode
 {
-	[SerializeField]
-	private string name = "";
-	[SerializeField]
-	private int[] parentIndices = new int[0];
+    [SerializeField]
+    private string name = "";
+    [SerializeField]
+    private int[] parentIndices = new int[0];
 
-	public TalentNode()
-	{
+    public TalentNode()
+    {
 
-	}
+    }
 
-	internal TalentNode(TalentNode original)
-	{
-		name = original.name; //fine because string is not mutable.
-		parentIndices = new int[original.parentIndices.Length];
-		for (int i = 0; i < parentIndices.Length; i++)
-		{
-			parentIndices[i] = original.parentIndices[i];
-		}
-	}
+    internal TalentNode(TalentNode original)
+    {
+        name = original.name; //fine because string is not mutable.
+        parentIndices = new int[original.parentIndices.Length];
+        for (int i = 0; i < parentIndices.Length; i++)
+        {
+            parentIndices[i] = original.parentIndices[i];
+        }
+    }
 
-	public string Name => name;
-	public int[] ParentIndices 
-	{
-		get => (int[])parentIndices.Clone(); 
-		internal set => parentIndices = value; 
-	}
+    public string Name => name;
+    public int[] ParentIndices
+    {
+        get => (int[])parentIndices.Clone();
+        internal set => parentIndices = value;
+    }
 
-	public bool HasParent => parentIndices.Length > 0;
-	public int RuntimeLevel { get; set; } = 0;
-	public RectTransform RuntimeInstance { get; set; }
-	public int NodeIndex { get; set; } = -1;
-	public NodeClass NodeClass { get; set; } = NodeClass.Unknown;
+    public bool HasParent => parentIndices.Length > 0;
+    public int RuntimeLevel { get; set; } = 0;
+    public RectTransform RuntimeInstance { get; set; }
+    public int NodeIndex { get; set; } = -1;
+    public NodeClass NodeClass { get; set; } = NodeClass.Unknown;
 
 }
 
 public enum NodeClass
 {
-	Unknown, Root, Inner, Leaf
+    Unknown, Root, Inner, Leaf
 }
